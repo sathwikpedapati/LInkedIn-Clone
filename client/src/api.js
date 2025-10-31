81e14c432c794ac2b37ee424c1cb892265f59597
sathwi_pedapati <161137317+sathwikpedapati@users.noreply.github.com>
2025-11-01 00:41:31 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://linkedin-clone-1-8jqm.onrender.com//api",
+  baseURL: "https://linkedin-clone-1-8jqm.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
